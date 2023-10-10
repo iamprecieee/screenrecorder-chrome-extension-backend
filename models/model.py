582f2ecb74e104c686e51c4e3e6164758448dd5c
Precious Iheanyi <emmypresh777@gmail.com>
2023-10-10 09:42:05 +0100

    added migrations, implemented saving transcript to database, added routes for creating and retrieving transcripts

--- models/model.py
+++ models/model.py
@@ -23,14 +23,16 @@ class Video(Base):
     resolution = Column(String)
     data = Column(LargeBinary)
     created_at = Column(TIMESTAMP, default=datetime.utcnow)
+    transcript = Column(String)
 
-    def __init__(self, filename, extension, size, resolution, data):
+    def __init__(self, filename, extension, size, resolution, data, transcript):
         self.id = str(uuid4())
         self.filename = filename
         self.data = data
         self.extension = extension
         self.size = size
         self.resolution = resolution
+        self.transcript = transcript
         models.storage.new(self)
 
     def save(self):
